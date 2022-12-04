299504e3fc6be2336c702ffe63a3bcd990c089a7
anveloper <hitedin@gmail.com>
2022-12-04 20:03:26 +0900

    chore: 프로젝트 초기 환경 설정

---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { store } from "./app/store";
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </HelmetProvider>
+);
