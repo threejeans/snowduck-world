5d848ce6af024c43407bd0504d95e66ae7947e49
anveloper <hitedin@gmail.com>
2022-12-07 15:56:48 +0900

    feat: 라우팅 페이지 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,21 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 // sample
 import { Counter } from "./features/counter/Counter";
 
 // page
-import Home from "./features/home/Home";
 import Login from "./features/auth/Login";
 import SignUp from "./features/auth/SignUp";
+import Home from "./features/home/Home";
 
 import styles from "./App.module.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className={styles.app}>
+      <Nav />
       "Snow Duck World"
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          gap: "8px",
-        }}
-      >
-        <Link to="/">홈</Link>
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/couter">state 샘플</Link>
-      </div>
       <Routes>
         <Route path="" element={<Home />} />
         <Route path="login" element={<Login />} />
