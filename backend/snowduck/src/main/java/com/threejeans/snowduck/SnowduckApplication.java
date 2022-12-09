546fe02e5324786ba87f357a6644e3142059465d
최진호 <84559471+turtlebooster@users.noreply.github.com>
2022-12-09 21:22:38 +0900

    feat: Spring Boot 초기 설정

---
+++ backend/snowduck/src/main/java/com/threejeans/snowduck/SnowduckApplication.java
@@ -0,0 +1,13 @@
+package com.threejeans.snowduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowduckApplication.class, args);
+    }
+
+}
